30d83e89b1ad1e2c37a9cf706233a46c2df74d8a
theprivatepark <anneypark@gmail.com>
2020-08-29 19:59:37 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+class Book
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre, :turn_page
 
+    def initialize(title)
+        @title = title
+    end
+
+    def properties
+        @title
+        @author
+        @page_count
+        @genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
